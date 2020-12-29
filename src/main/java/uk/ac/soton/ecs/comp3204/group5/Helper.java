1c4a040a6e3ddfb17b0104d0a6ba9114f1022491
peter <phj1g18@soton.ac.uk>
2020-12-29 13:45:27 +0000

    Run 2 accuracy acceptable, run 2 code tidied up and commented

--- src/main/java/uk/ac/soton/ecs/comp3204/group5/Helper.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/group5/Helper.java
@@ -16,42 +16,58 @@ import java.util.List;
 import java.util.Map;
 
 public class Helper {
-
+    //probably deperecated class, does the same thing as the other one but for FloatKeypoint objects
     public static LocalFeatureList<FloatKeypoint> createLocalFeatureList(FImage image) {
         ArrayList<FloatKeypoint> keypoints = new ArrayList<>();
         RectangleSampler sampler = new RectangleSampler(image.normalise(),4,4,8,8);
         List<Rectangle> rectangles = sampler.allRectangles();
         for(Rectangle r : rectangles) {
-            FImage rImg = image.normalise().extractROI(r);
+            FImage rImg = image.extractROI(r).normalise();
             float[] rVector = rImg.getFloatPixelVector();
             keypoints.add(new FloatKeypoint(r.x, r.y, 0, 1, rVector));
         }
         return new MemoryLocalFeatureList<>(keypoints);
     }
 
-    public static LocalFeatureList<LocalFeatureImpl<SpatialLocation, FloatFV>> getFeatures(FImage image) {
-        ArrayList<LocalFeatureImpl<SpatialLocation, FloatFV>> features = new ArrayList<>();
+    /**
+     * Creates a local feature list of densely sampled pixel patches flattened into a vector
+     * @param image - the image to be sampled
+     * @return - a local feature list
+     */
+    public static LocalFeatureList<LocalFeatureImpl<SpatialLocation, FloatFV>> getPixelPatchFeatures(FImage image) {
+        LocalFeatureList<LocalFeatureImpl<SpatialLocation, FloatFV>> features = new MemoryLocalFeatureList<>();
+        //create 8x8 pixel patches sampled every 4 pixels in the x and y directions
         RectangleSampler sampler = new RectangleSampler(image.normalise(),4,4,8,8);
         List<Rectangle> rectangles = sampler.allRectangles();
         for(Rectangle r : rectangles) {
-            FImage rImg = image.normalise().extractROI(r);
+            //create an image from the given patch, then normalise it
+            FImage rImg = image.extractROI(r).normalise();
+            //flatten patch into vector
             float[] rVector = rImg.getFloatPixelVector();
-            //keypoints.add(new FloatKeypoint(r.x, r.y, 0, 1, rVector));
+            //create local feature from vector and location
             SpatialLocation rLoc = new SpatialLocation(r.x, r.y);
             features.add(new LocalFeatureImpl<>(rLoc, new FloatFV(rVector)));
         }
-        return new MemoryLocalFeatureList<>(features);
+        return features;
     }
+
+    /**
+     * Create a GroupedDataset from a VFSGroupDataset by creating a Record from each image and its class
+     * @param originalDataset - the dataset that should be converted
+     * @return a GroupedDataset
+     */
     public static GroupedDataset<String, ListBackedDataset<Record>, Record> convertToGroupedDataset(VFSGroupDataset<FImage> originalDataset) {
         GroupedDataset<String, ListBackedDataset<Record>, Record> recordDataset = new MapBackedDataset<>();
-        //iterate through each class in original dataset, create Record objects from images in each class
+        //iterate through each class in original dataset
         for (final Map.Entry<String, VFSListDataset<FImage>> entry : originalDataset.entrySet()) {
+            //create a ListBackedDataset of each image class
             VFSListDataset<FImage> imageList = entry.getValue();
             ListBackedDataset<Record> recordList = new ListBackedDataset<>();
             for (int i = 0; i < imageList.size(); i++) {
                 FImage image = imageList.get(i);
                 recordList.add(new Record(image, i+"", entry.getKey()));
             }
+            //add to GroupedDataset
             recordDataset.put(entry.getKey(), recordList);
         }
         return recordDataset;
--- src/main/java/uk/ac/soton/ecs/comp3204/group5/run2/App.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/group5/run2/App.java
@@ -17,13 +17,9 @@ import org.openimaj.feature.local.LocalFeatureImpl;
 import org.openimaj.feature.local.SpatialLocation;
 import org.openimaj.feature.local.data.LocalFeatureListDataSource;
 import org.openimaj.feature.local.list.LocalFeatureList;
-import org.openimaj.feature.local.list.MemoryLocalFeatureList;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
-import org.openimaj.image.feature.local.keypoints.FloatKeypoint;
-import org.openimaj.image.pixel.sampling.RectangleSampler;
-import org.openimaj.math.geometry.shape.Rectangle;
 import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
 import org.openimaj.ml.clustering.FloatCentroidsResult;
 import org.openimaj.ml.clustering.assignment.HardAssigner;
@@ -39,47 +35,21 @@ import java.util.Map;
 public class App {
     private static final String filepath1 = "C:\\Users\\peter\\Downloads\\training";
     public static void main( String[] args ) throws FileSystemException {
-
+        //load dataset from file
         VFSGroupDataset<FImage> originalDataset = new VFSGroupDataset<>( filepath1, ImageUtilities.FIMAGE_READER);
         System.out.println("LOADED DATASET");
+        //convert to GroupedDataset to provide more functionality
         GroupedDataset<String, ListBackedDataset<Record>, Record> recordDataset = Helper.convertToGroupedDataset(originalDataset);
         System.out.println("POPULATED RECORD DATASET");
-
         //GroupedDataset<String, ListDataset<Record>, Record> sampledData = GroupSampler.sample(recordDataset, 5, false);
 
         GroupedRandomSplitter<String, Record> splitData = new GroupedRandomSplitter<>(recordDataset, 80, 0, 20);
+        System.out.println("SPLIT DATASET INTO TRAINING AND TESTING");
 
-        System.out.println("SAMPLED DATASET");
-
-        List<float[]> vectorList = new ArrayList<>();
-        List<LocalFeatureImpl<SpatialLocation, FloatFV>> sampleFeatures = new ArrayList<>();
-        // iterate through samples, create fixed size densely-sampled pixel patches from their normalised form
-        /*
-        for(Record record : GroupedUniformRandomisedSampler.sample(splitData.getTrainingDataset(), 15)) {
-
-            RectangleSampler sampler = new RectangleSampler(record.getImage().normalise(),4,4,8,8);
-            List<Rectangle> patchesOfRecord = sampler.allRectangles();
-            // take the pixels from the patches, flatten them into a vector, then add the vector to a list
-
-            for(Rectangle patch : patchesOfRecord) {
-                FImage img = record.getImage().normalise().extractROI(patch);
-                float[] imgVector = img.getFloatPixelVector();
-                vectorList.add(imgVector);
-                SpatialLocation location = new SpatialLocation(patch.x, patch.y);
-                sampleFeatures.add(new LocalFeatureImpl<SpatialLocation, FloatFV>(location, new FloatFV(imgVector)));
-            }
-
-        }
-        */
-        //float[][] patchVectors = convertToArr(vectorList); System.out.println("CREATED PATCHES");
-        // from the patch vectors created above, create an assigner
-        //HardAssigner<float[], float[], IntFloatPair> assigner = createVocabulary(patchVectors); System.out.println("CLUSTERED PATCHES");
-
-        //LocalFeatureList<LocalFeatureImpl<SpatialLocation, FloatFV>> sampleLfl = new MemoryLocalFeatureList<>(sampleFeatures);
         HardAssigner<float[], float[], IntFloatPair> assigner = trainQuantiser(GroupedUniformRandomisedSampler.sample(splitData.getTrainingDataset(), 30));
-
+        System.out.println("CLUSTERING DONE");
         // use assigner to create a FeatureExtractor
-        FeatureExtractor<SparseIntFV,Record> extractor = new Extractor(assigner); System.out.println("EXTRACT FEATURES");
+        FeatureExtractor<SparseIntFV,Record> extractor = new BOVWExtractor(assigner);
         // use FeatureExtractor to create classifier
         LiblinearAnnotator<Record, String> ann =
                 new LiblinearAnnotator<>(extractor, LiblinearAnnotator.Mode.MULTICLASS, SolverType.L2R_L2LOSS_SVC, 1.0, 0.00001);
@@ -96,54 +66,34 @@ public class App {
         System.out.println(result);
     }
 
-    private static float[][] convertToArr(List<float[]> vectors) {
-        float[][] floatArr = new float[vectors.size()][];
-        for(int i = 0; i < vectors.size(); i++) {
-            floatArr[i] = vectors.get(i);
-        }
-        return floatArr;
-    }
-
-    static HardAssigner<float[], float[], IntFloatPair> createClusters(LocalFeatureList<LocalFeatureImpl<SpatialLocation, FloatFV>> sample) {
-        DataSource<float[]> dataSource = new LocalFeatureListDataSource<>(sample);
-        FloatKMeans kMeans = FloatKMeans.createKDTreeEnsemble(500);
-        FloatCentroidsResult result = kMeans.cluster(dataSource);
-        return  result.defaultHardAssigner();
-    }
-
+    /**
+     * Creates a vocabulary of visual words from a sample of images using K-means clustering
+     * @param sample - a subset of the training dataset
+     * @return a vocabulary of visual words
+     */
     static HardAssigner<float[], float[], IntFloatPair> trainQuantiser(GroupedDataset<String, ListDataset<Record>, Record> sample) {
         List<LocalFeatureList<LocalFeatureImpl<SpatialLocation, FloatFV>>> allKeys = new ArrayList<>();
         for(Record record : sample) {
-            allKeys.add(Helper.getFeatures(record.getImage()));
+            allKeys.add(Helper.getPixelPatchFeatures(record.getImage()));
         }
 
         if (allKeys.size() > 10000) {
             allKeys = allKeys.subList(0, 10000);
         }
-        FloatKMeans kMeans = FloatKMeans.createKDTreeEnsemble(500);
+        FloatKMeans kMeans = FloatKMeans.createExact(600);
         DataSource<float[]> dataSource = new LocalFeatureListDataSource<>(allKeys);
         FloatCentroidsResult result = kMeans.cluster(dataSource);
         return  result.defaultHardAssigner();
     }
 
     /**
-     * Learn a vocabulary by clustering a sample of pixel patches
-     * @param patches - an array of float vectors of patches of sample images
-     * @return - a default Hard Assigner created from applying Kmeans to the input
+     * Implementation of a Feature Extractor which uses a HardAssigner to create a BagOfVisualWords feature
+     * which extracts features from densely sampled pixel patches
      */
-    static HardAssigner<float[], float[], IntFloatPair> createVocabulary(float[][] patches) {
-        FloatKMeans kMeans = FloatKMeans.createExact(500);
-        FloatCentroidsResult result = kMeans.cluster(patches);
-        return result.defaultHardAssigner();
-    }
-
-    /**
-     * WIP class for a Feature Extractor which uses a HardAssigner to create a BagOfVisualWords feature
-     */
-    static class Extractor implements FeatureExtractor<SparseIntFV, Record> {
+    static class BOVWExtractor implements FeatureExtractor<SparseIntFV, Record> {
         BagOfVisualWords<float[]> bagOfVisualWords;
         HardAssigner<float[], float[], IntFloatPair> assigner;
-        public Extractor(HardAssigner<float[], float[], IntFloatPair> assigner) {
+        public BOVWExtractor(HardAssigner<float[], float[], IntFloatPair> assigner) {
             this.assigner = assigner;
             this.bagOfVisualWords = new BagOfVisualWords<>(assigner);
         }
@@ -151,7 +101,7 @@ public class App {
         @Override
         public SparseIntFV extractFeature(Record record) {
             FImage image = record.getImage();
-            return bagOfVisualWords.aggregate(Helper.getFeatures(image));
+            return bagOfVisualWords.aggregate(Helper.getPixelPatchFeatures(image));
         }
     }
 }
